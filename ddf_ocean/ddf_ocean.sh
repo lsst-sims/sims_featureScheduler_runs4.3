816bd8ab95a87cf5f20de93781ca8ffcc0f097f5
Peter Yoachim <yoachim@uw.edu>
2025-05-27 12:14:03 -0700

    ocean

--- ddf_ocean/ddf_ocean.sh
+++ ddf_ocean/ddf_ocean.sh
@@ -2,3 +2,4 @@ python ddf_ocean.py --ddf_config_file ocean1.dat
 python ddf_ocean.py --ddf_config_file ocean2.dat
 python ddf_ocean.py --ddf_config_file ocean3.dat
 python ddf_ocean.py --ddf_config_file ocean4.dat
+python ../baseline/baseline.py 
